8a0aed876822c9dd0539df8bca7d1f8f29bf0aca
이기창 <leepository@gmail.com>
2026-01-21 13:58:09 +0900

    Add files via upload

---
+++ erp/src/main/java/port/sm/erp/dto/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package port.sm.erp.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+	
+	private String email;
+	private String password;
+
+}
